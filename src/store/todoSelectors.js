d6053aabe408f978f608b7aba3293685ed6ef467
name <61285938+yuriiyablonskyi@users.noreply.github.com>
2024-01-19 15:47:12 +0100

    Removed selector (#2)
    * Remove selector
    
    * Add refactor
    
    ---------
    
    Co-authored-by: Anastasiia Masliukh <deggetk@gmail.com>

--- src/store/todoSelectors.js
+++ src/store/todoSelectors.js
@@ -1,3 +1,2 @@
 export const userData = state => state.user.user
-export const tasksData = state => state.tasks.tasks
-export const statusTasksData = state => state.tasks.loading
+export const tasksData = state => state.tasks
